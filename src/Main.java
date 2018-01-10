e78d64eb0928474c7cd33f9fdbf8c2633c54fcfd
kang <licheeking@gmail.com>
2018-01-10 15:49:35 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    // 设置一个全局动态数组，来存放文件路径
+    // 主要遍历文件夹，包含所有子文件夹、文件的情况时，用到递归，所以要这样设置
+    public static ArrayList<String> dirAllStrArr = new ArrayList<String>();
+
+    // 这里是仅仅查询当前路径下的所有文件夹、文件并且存放其路径到文件数组
+    // 由于遇到文件夹不查询其包含所有子文件夹、文件，因此没必要用到递归
+    public static ArrayList<String> Dir(File dirFile) throws Exception {
+        ArrayList<String> dirStrArr = new ArrayList<String>();
+
+        if (dirFile.exists()) {
+            // 直接取出利用listFiles()把当前路径下的所有文件夹、文件存放到一个文件数组
+            File files[] = dirFile.listFiles();
+            for (File file : files) {
+                // 如果传递过来的参数dirFile是以文件分隔符，也就是/或者\结尾，则如此构造
+                if (dirFile.getPath().endsWith(File.separator)) {
+                    dirStrArr.add(dirFile.getPath() + file.getName());
+                } else {
+                    // 否则，如果没有文件分隔符，则补上一个文件分隔符，再加上文件名，才是路径
+                    dirStrArr.add(dirFile.getPath() + File.separator
+                            + file.getName());
+                }
+            }
+        }
+        return dirStrArr;
+    }
+
+
+
+    public static void DirAll(File dirFile) throws Exception {
+
+        if (dirFile.exists()) {
+            File files[] = dirFile.listFiles();
+            for (File file : files) {
+                // 如果遇到文件夹则递归调用。
+                if (file.isDirectory()) {
+                    // 递归调用
+                    DirAll(file);
+                } else {
+                    // 如果遇到文件夹则放入数组
+                    if (dirFile.getPath().endsWith(File.separator)) {
+                        dirAllStrArr.add(dirFile.getPath() + file.getName());
+                    } else {
+                        dirAllStrArr.add(dirFile.getPath() + File.separator
+                                + file.getName());
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean readfile(String filepath) throws Exception {
+        try {
+
+            File file = new File(filepath);
+            if (!file.isDirectory()) {
+//                System.out.println("文件");
+//                System.out.println("path=" + file.getPath());
+//                System.out.println("absolutepath=" + file.getAbsolutePath());
+                System.out.println("name=" + file.getName());
+
+            } else if (file.isDirectory()) {
+//                System.out.println("文件夹");
+                String[] filelist = file.list();
+                for (int i = 0; i < filelist.length; i++) {
+                    File readfile = new File(filepath + "\\" + filelist[i]);
+                    if (!readfile.isDirectory()) {
+//                        System.out.println("path=" + readfile.getPath());
+//                        System.out.println("absolutepath="
+//                                + readfile.getAbsolutePath());
+                        System.out.println(readfile.getName());
+
+                    } else if (readfile.isDirectory()) {
+                        readfile(filepath + "\\" + filelist[i]);
+                    }
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("readfile()   Exception:" + e.getMessage());
+        }
+        return true;
+    }
+
+    public static String filepath =
+    "D:\\12.1 OFFSHORE\\F0033(E21A.02)-GTC\\Document Transmittal\\F0033-GTC-0026-T";
+
+    @Test
+    public void Name() throws Exception {
+        readfile(filepath);
+    }
+
+    public static void main(String[] args) throws Exception {
+        File dirFile = new File(filepath);
+
+        DirAll(dirFile);
+        for (String str:
+             dirAllStrArr) {
+            System.out.println(str);
+        }
+//        System.out.println(dirAllStrArr);
+    }
+}
